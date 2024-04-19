a92e96824a2c09bde9ace61d890e05a0fae61748
ivan <i.razgonyaev@estesis.tech>
2024-04-19 15:02:53 +0700

    refactor(pg_timezone): Изменил тип поля created_at с timestamp with timezone на timestamp without timezone.

---
+++ migration/20240419071927_alter_bash_log_created_at_column.sql
@@ -0,0 +1,17 @@
+-- +goose Up
+-- +goose StatementBegin
+ALTER TABLE IF EXISTS
+    scripts.bash_log
+ALTER COLUMN
+    created_at
+TYPE TIMESTAMP WITHOUT TIME ZONE;
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+ALTER TABLE IF EXISTS
+    scripts.bash_log
+ALTER COLUMN
+    created_at
+TYPE TIMESTAMP WITH TIME ZONE;
+-- +goose StatementEnd
