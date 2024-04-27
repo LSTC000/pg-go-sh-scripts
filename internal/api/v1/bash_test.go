c953948ea01f0f991847000443beb9773c9ce10e
ivan <i.razgonyaev@estesis.tech>
2024-04-27 11:36:47 +0700

    feature(test): Написал тесты для проверки функционала удаления bash скрипта.

--- internal/api/v1/bash_test.go
+++ internal/api/v1/bash_test.go
@@ -2,7 +2,6 @@ package v1
 
 import (
 	"errors"
-	uuid "github.com/satori/go.uuid"
 	"net/http"
 	"net/http/httptest"
 	mock_api "pg-sh-scripts/internal/api/mock"
@@ -15,6 +14,8 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+
+	uuid "github.com/satori/go.uuid"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
@@ -298,6 +299,114 @@ func TestBashHandler_GetBashList(t *testing.T) {
 				requestQueryParams.Add("offset", strconv.Itoa(testCase.in.paginationParams.Offset))
 			}
 			request.URL.RawQuery = requestQueryParams.Encode()
+
+			r.ServeHTTP(recorder, request)
+
+			assert.Equal(t, testCase.expected.code, recorder.Code)
+			assert.Equal(t, testCase.expected.body, recorder.Body.String())
+		})
+	}
+}
+
+func TestBashHandler_RemoveBashById(t *testing.T) {
+	type (
+		inStruct struct {
+			bashId  string
+			httpErr error
+		}
+
+		expectedStruct struct {
+			body string
+			code int
+		}
+	)
+
+	httpErrors := config.GetHTTPErrors()
+
+	testCases := []struct {
+		name         string
+		in           inStruct
+		mockBehavior func(*mock_usecase.MockIBashUseCase, *mock_api.MockIHelper, uuid.UUID, error)
+		expected     expectedStruct
+	}{
+		{
+			name: "Success",
+			in: inStruct{
+				bashId:  uuid.NewV4().String(),
+				httpErr: nil,
+			},
+			mockBehavior: func(mu *mock_usecase.MockIBashUseCase, mh *mock_api.MockIHelper, bashId uuid.UUID, err error) {
+				mu.EXPECT().RemoveBashById(bashId).Return(&model.Bash{}, nil)
+			},
+			expected: expectedStruct{
+				body: `{"id":"00000000-0000-0000-0000-000000000000","title":"","body":"","createdAt":"0001-01-01T00:00:00Z"}`,
+				code: http.StatusOK,
+			},
+		},
+		{
+			name: "Bash id must be uuid error",
+			in: inStruct{
+				bashId:  "uuid",
+				httpErr: httpErrors.BashId,
+			},
+			mockBehavior: func(mu *mock_usecase.MockIBashUseCase, mh *mock_api.MockIHelper, bashId uuid.UUID, err error) {
+				var httpErr *schema.HTTPError
+				errors.As(err, &httpErr)
+				mh.EXPECT().ParseError(err).Return(httpErr)
+			},
+			expected: expectedStruct{
+				body: `{"httpCode":422,"serviceCode":200,"detail":"The bash id must be of type uuid4 like 151a583c-0ea0-46b8-b8a6-6bdcdd51655a"}`,
+				code: http.StatusUnprocessableEntity,
+			},
+		},
+		{
+			name: "Removing bash error",
+			in: inStruct{
+				bashId:  uuid.NewV4().String(),
+				httpErr: httpErrors.BashRemove,
+			},
+			mockBehavior: func(mu *mock_usecase.MockIBashUseCase, mh *mock_api.MockIHelper, bashId uuid.UUID, err error) {
+				var httpErr *schema.HTTPError
+				errors.As(err, &httpErr)
+
+				gomock.InOrder(
+					mu.EXPECT().RemoveBashById(bashId).Return(nil, err),
+					mh.EXPECT().ParseError(err).Return(httpErr),
+				)
+			},
+			expected: expectedStruct{
+				body: `{"httpCode":400,"serviceCode":211,"detail":"An error occurred while deleting the bash script"}`,
+				code: http.StatusBadRequest,
+			},
+		},
+	}
+
+	gin.SetMode(gin.TestMode)
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			mockBashUseCase := mock_usecase.NewMockIBashUseCase(ctrl)
+			mockApiHelper := mock_api.NewMockIHelper(ctrl)
+			uuidBashId, _ := uuid.FromString(testCase.in.bashId)
+			testCase.mockBehavior(mockBashUseCase, mockApiHelper, uuidBashId, testCase.in.httpErr)
+
+			bashHandler := BashHandler{
+				useCase:    mockBashUseCase,
+				helper:     mockApiHelper,
+				httpErrors: httpErrors,
+			}
+
+			path := groupBashPath + removeBashPath
+			casePath := strings.Replace(path, ":id", testCase.in.bashId, 1)
+
+			r := gin.New()
+			r.DELETE(path, bashHandler.RemoveBashById)
+
+			recorder := httptest.NewRecorder()
+			request := httptest.NewRequest(http.MethodDelete, casePath, nil)
 
 			r.ServeHTTP(recorder, request)
 
